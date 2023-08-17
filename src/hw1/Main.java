c1760558b33240bc3728c1951a3858a817dc6dc8
AlexanderIsai <74790416+AlexanderIsai@users.noreply.github.com>
2023-08-17 19:58:40 +0300

    javaPro-hw1

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -11,10 +11,5 @@ public class Main {
         Person secondPerson = new Person("Hanna", 30, 165, 60, 200);
         Person thirdPerson = new Person("Oleg", 42, 175, 84, 300);
         Person forthPerson = new Person("Daniil", 72, 177, 73, 400);
-
-        System.out.println(firstPerson.howMuchMoneyHasPerson());
-        System.out.println(secondPerson.howMuchMoneyHasPerson());
-        System.out.println(forthPerson.howMuchMoneyHasPerson());
-
     }
 }
