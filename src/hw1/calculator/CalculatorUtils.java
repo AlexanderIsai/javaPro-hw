3fe9733aee265e4084715daf294308bfd2a4421a
AlexandrIsai <artat@ukr.net>
2023-09-04 21:40:25 +0300

    hw5

--- src/hw1/calculator/CalculatorUtils.java
+++ src/hw1/calculator/CalculatorUtils.java
@@ -4,10 +4,10 @@ public class CalculatorUtils {
 
 
     public static double calculateAverage(int first, int second) {
-        return Math.round((double) (first + second) / 2 * 100 / 100);
+        return (first + second) / 2.0;
     }
 
     public static double calculateAverage(int first, int second, int third) {
-        return Math.round((double) (first + second + third) / 3 * 100 / 100);
+        return (first + second + third) / 3.0;
     }
 }
