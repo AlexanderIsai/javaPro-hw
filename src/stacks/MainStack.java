6960559897067d705a03b3a721fd76ec4334846e
AlexandrIsai <artat@ukr.net>
2023-10-02 21:17:03 +0300

    hw9

--- src/stacks/MainStack.java
+++ src/stacks/MainStack.java
@@ -11,21 +11,21 @@ public class MainStack {
         encryption(string, secret);
     }
 
-    public static void encryption(String str, int secret){
-        Stack<Character> characterStack= new Stack<>();
+    public static void encryption(String str, int secret) {
+        Stack<Character> characterStack = new Stack<>();
         int fullWord = str.length() / secret;
-        if (str.length() % secret != 0){
+        if (str.length() % secret != 0) {
             fullWord++;
         }
         String[] strings = new String[fullWord];
         int i = 0;
-            for(int j = 0; j < strings.length; j++){
-                if((i + secret) > str.length()){
-                    strings[strings.length - 1] = str.substring((fullWord - 1) * secret);
-                } else {
-                    strings[j] = str.substring(i, i+=secret);
-                }
+        for (int j = 0; j < strings.length; j++) {
+            if ((i + secret) > str.length()) {
+                strings[strings.length - 1] = str.substring((fullWord - 1) * secret);
+            } else {
+                strings[j] = str.substring(i, i += secret);
             }
+        }
         for (String string : strings) {
             for (int n = 0; n < string.length(); n++) {
                 characterStack.push(string.charAt(n));
